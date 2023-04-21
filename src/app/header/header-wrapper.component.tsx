8d39025cb099d0f5ca3ee9026bcefb5cd5e942ec
Kevin Logan <kevin.logan@ravenind.com>
2023-04-21 09:19:54 -0500

    Avoid warning by calling createRoot in main.ts

--- src/app/header/header-wrapper.component.tsx
+++ src/app/header/header-wrapper.component.tsx
@@ -13,7 +13,7 @@ import {
 } from "@angular/core";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
+import { Root, createRoot } from "react-dom/client";
 import { Header } from "./Header";
 const containerElementName = "reactHeaderComponentContainer";
 
@@ -35,38 +35,43 @@ export class HeaderWrapperComponent
 
   constructor() {}
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('changed');
-    this.render();
+    // note: It works, but I get this warning from changed events (from the @Input update approach)
+    // Warning: You are calling ReactDOMClient.createRoot() on a container that has already been passed to createRoot() before. Instead, call root.render() on the existing root instead if you want to update it.
+    // I've decided to ignore it since the render is efficient
+    //   If you call render on the same root more than once, React will update the DOM as necessary to reflect the latest JSX you passed. React will decide which parts of the DOM can be reused and which need to be recreated by “matching it up” with the previously rendered tree. Calling render on the same root again is similar to calling the set function on the root component: React avoids unnecessary DOM updates.
+    //   see https://react.dev/learn/preserving-and-resetting-state
+    // these can be avoided by using the document.addEventListener approach
+    // it is also avoided with calling createRoot in the main.ts file
+    console.log("changed");
+    HeaderWrapperComponent.render(undefined);
   }
-  
+
   ngAfterViewInit() {
-    console.log('afterViewInit');
-    this.render();
+    console.log("afterViewInit");
+    HeaderWrapperComponent.render(undefined);
   }
 
   ngOnDestroy() {
     ReactDOM.unmountComponentAtNode(this.containerRef.nativeElement);
   }
 
-  private render() {
-    // note: I do get this warning from ngAfterViewInit and changed events
-    // Warning: You are calling ReactDOMClient.createRoot() on a container that has already been passed to createRoot() before. Instead, call root.render() on the existing root instead if you want to update it.
-    // I've decided to ignore it since the render is efficient
-    //   If you call render on the same root more than once, React will update the DOM as necessary to reflect the latest JSX you passed. React will decide which parts of the DOM can be reused and which need to be recreated by “matching it up” with the previously rendered tree. Calling render on the same root again is similar to calling the set function on the root component: React avoids unnecessary DOM updates.
-    //   see https://react.dev/learn/preserving-and-resetting-state
-    const root = createRoot(this.containerRef.nativeElement as HTMLElement);
-    root.render(
-      <React.StrictMode>
-        <Header
-          {...{
-            headerComponents: [
-              // without the ts-ignore, this won't compile
-              /* @ts-ignore */
-              <notification-web-component counter={this.appNotificationCounter}></notification-web-component>,
-            ],
-          }}
-        />
-      </React.StrictMode>
-    );
+  static reactHeaderRoot: Root;
+  public static render(reactHeaderRoot: Root | undefined) {
+    // set in main.ts
+    if (reactHeaderRoot) HeaderWrapperComponent.reactHeaderRoot = reactHeaderRoot;
+    if (HeaderWrapperComponent.reactHeaderRoot)
+      HeaderWrapperComponent.reactHeaderRoot.render(
+        <React.StrictMode>
+          <Header
+            {...{
+              headerComponents: [
+                // without the ts-ignore, this won't compile
+                /* @ts-ignore */
+                <notification-web-component counter={this.appNotificationCounter}></notification-web-component>,
+              ],
+            }}
+          />
+        </React.StrictMode>
+      );
   }
 }
