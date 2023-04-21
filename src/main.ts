5acef43a3ef0c730ccd9d766c51959553c29933d
Kevin Logan <kevin.logan@ravenind.com>
2023-04-21 10:33:16 -0500

    Revert "Avoid warning by calling createRoot in main.ts"
    This reverts commit 8d39025cb099d0f5ca3ee9026bcefb5cd5e942ec.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { createRoot } from 'react-dom/client';
-import { HeaderWrapperComponent } from './app/header/header-wrapper.component';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-  // this is a way to avoid the 
-  // Warning: You are calling ReactDOMClient.createRoot() on a container that has already been passed to createRoot() before. Instead, call root.render() on the existing root instead if you want to update it.
-  // from @input changes
-  // poll dom until this element is available
-  function pollForHeader() {
-    if (document.querySelector('app-header-wrapper')) {
-      const header = document.querySelector('app-header-wrapper') as Element;
-      const root = createRoot(header); 
-      HeaderWrapperComponent.render(root);
-    } else {
-      setTimeout(pollForHeader, 100);
-    }
-  }
-  pollForHeader();
